38080a6bc11dae18ce290b674265d2fb8ff67750
anandhan0704 <60783032+anandhan0704@users.noreply.github.com>
2021-08-31 20:00:42 +0530

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+dbPassword = 'mongodb+srv://anandhan:'+ encodeURIComponent('anandhan0704') + '@cluster0.hxoon.mongodb.net/login?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
+
